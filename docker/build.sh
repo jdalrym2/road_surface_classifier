3adedb62e4d46d9dd8d19a6f50416a673e73ed72
j_dalrym2 <j_dalrym2@hotmail.com>
2023-01-08 23:11:53 -0700

    Finished docker updates

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TAG=truenas.local:9909/road_surface_classifier:latest  # set this to whatever you want
-DOCKER_BUILDKIT=1 docker build -t $TAG .
+DOCKER_BUILDKIT=1 dBUILDKIT_PROGRESS=plain docker build -t $TAG .
