2d79168abfb8e62f00d989ee68803ba7b88c1c26
j_dalrym2 <j_dalrym2@hotmail.com>
2023-02-26 18:50:41 -0700

    Consolidate loss functions, just to cleanup some

--- rsc/model/mcnn_loss.py
+++ rsc/model/mcnn_loss.py
@@ -1,49 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import numpy as np
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as functional
-
-from .dice_bce_loss import DiceBCELoss
-
-
-def custom_sigmoid(d: torch.Tensor,
-                   x_range: tuple[float, float] = (0, 1),
-                   y_range: tuple[float, float] = (0, 1),
-                   k: float = 0.1) -> torch.Tensor:
-    """
-    Create a custom sigmoid (logit: `1 / (1 + exp(x))`), in
-    which the transition occurs approximately within `x_range`, between
-    the values in `y_range`
-
-    Args:
-        d (torch.Tensor): Input data
-        x_range (tuple[float, float], optional): X- domain. Defaults to (0, 1).
-        y_range (tuple[float, float], optional): Y- domain. Defaults to (0, 1).
-        k (float, optional): Slope. Smaller `k` -> bigger slope. Defaults to 0.1.
-
-    Returns:
-        torch.Tensor: Sigmoid output
-    """
-    # Parse input range
-    x_min, x_max = x_range
-    y_min, y_max = y_range
-
-    # Normalize d to [0, 1]
-    d = (d - x_min) / (x_max - x_min)
-
-    # Now normalize to [-1, 1]
-    d = 2 * (d - 0.5)
-
-    # Compute sigmoid
-    s = 1 / (1 + torch.exp(-d / k))
-
-    # Normalize to y_range
-    s = s * (y_max - y_min) + y_min
-
-    return s
 
 
 class MCNNLoss(nn.Module):
@@ -52,32 +12,53 @@ class MCNNLoss(nn.Module):
         super().__init__()
         self.class_weights = torch.Tensor(class_weights).float().cuda()
         self.loss_lambda = loss_lambda
-        self.dice_loss = DiceBCELoss()
+        self.smooth = 1     # Dice-BCE loss smoothing parameter, will just hardcode to 1
 
         self.loss1 = 0.
         self.loss2 = 0.
         self.stage = 0
 
-    def forward(self, y_hat, y, z_hat, z):
+    def forward(self, y_hat: torch.Tensor, y: torch.Tensor,
+                z_hat: torch.Tensor, z: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the loss given the model's segmentation and classification results.
 
+        Segmentation loss is DICE + BCE loss, inspired from:
+        https://www.kaggle.com/code/bigironsphere/loss-function-library-keras-pytorch/notebook#BCE-Dice-Loss
+
+        Args:
+            y_hat (torch.Tensor): Segmentation model result
+            y (torch.Tensor): Segmentation truth
+            z_hat (torch.Tensor): Classification model result
+            z (torch.Tensor): Classification truth
+
+        Returns:
+            torch.Tensor: Computed loss for the model
+        """
+
+        # Loss 1: Dice BCE loss for segmentation
         if self.stage in (0, 1):
-            self.loss1 = self.dice_loss(y_hat, y)
+            intersection = (y_hat * y).sum()
+            dice_loss = 1 - (2. * intersection + self.smooth) / (
+                y_hat.sum() + y.sum() + self.smooth)
+            binary_ce = functional.binary_cross_entropy(y_hat,
+                                                        y,
+                                                        reduction='mean')
+            self.loss1 = binary_ce + dice_loss
         else:
             self.loss1 = 0.
 
+        # Loss 2: Cross entropy for classification result
         if self.stage in (0, 2):
-            # Compute weight based on "true" obscuaration
-            # Fit a sigmoid from 1->0.1 between 0.9->1 in range
-            # This will punish the model less for obscuration values
-            # >= 0.9
-            #obsc_w = custom_sigmoid(z[..., 2], (0.4, 0.9), (1, 5), 0.15)
-
             self.loss2 = functional.cross_entropy(
-                z_hat, z, weight=self.class_weights,
-                reduction='mean')     # reduction='none'
-            #self.loss2 = torch.sum(obsc_w * self.loss2) / torch.sum(obsc_w)
+                z_hat,
+                z,
+                weight=self.class_weights,
+            )
+
         else:
             self.loss2 = 0.
 
+        # Combine and return the combined loss
         loss = self.loss_lambda * self.loss1 + self.loss2
         return loss
